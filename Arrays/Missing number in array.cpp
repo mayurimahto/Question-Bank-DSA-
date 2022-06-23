cdff23f39f895b6589fc9bf17401801b8249f321
Mayuri Mahto <mahtomayuri@gmail.com>
2022-06-23 20:05:31 +0530

    ques4

--- Arrays/Missing number in array.cpp
+++ Arrays/Missing number in array.cpp
@@ -48,3 +48,54 @@ int main() {
 }  // } Driver Code Ends
 
 
+//Method 3
+// XOR
+
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+ // } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        //XOR 
+        
+        int ans=0;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            ans^=array[i];
+        }
+        for(int i=1; i<n+1; i++)
+        {
+            ans^=i;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
