3880944e5fc3e5fc1f1c5cd2392e0900d9447953
Mayuri Mahto <mahtomayuri@gmail.com>
2022-06-23 20:08:36 +0530

    3rd ques

---
+++ Arrays/Kadane's Algorithm.cpp
@@ -0,0 +1,59 @@
+// Bruteforce - Iterate over all subarrays and store the sums and maintain a maxsum
+
+//Kadanes Algorithm
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        //Kadane Algorithm
+        
+        int sum =0;
+        int maxsum = INT_MIN; //arr[0]
+        
+        for(int i=0; i<n; i++)
+        {
+            sum+= arr[i];
+            maxsum = max(maxsum, sum);
+            
+            if(sum<0)
+            {
+                sum = 0;
+            }
+        }
+        return maxsum;
+    } 
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
