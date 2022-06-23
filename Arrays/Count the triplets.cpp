8a821fd574e5907c12574729b693e2d716edbbf1
Mayuri Mahto <mahtomayuri@gmail.com>
2022-06-23 14:19:44 +0530

    2nd ques

--- Arrays/Count the triplets.cpp
+++ Arrays/Count the triplets.cpp
@@ -64,3 +64,71 @@ int main()
     }
     return 0;
 }
+
+
+//Using Hashmap
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int countTriplet(int arr[], int n)
+	{
+	    // Your code goes here
+	    //Using Hashmap
+	    
+	    unordered_map<int, int>memo;
+	    int count=0;
+	    
+	    for(int i=0; i<n; i++)
+	    memo[arr[i]]++;
+	    
+	    for(int l=0; l<n-1; l++)
+	    {
+	        for(int r=l+1; r<n; r++)
+	        {
+	            if(memo.find(arr[l]+arr[r])!=memo.end())
+	            {
+	            count++;
+	            }
+	        }
+	    }
+	    return count;
+	}
+};
+
+// { Driver Code Starts.
+
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        	cin >> arr[i];
+
+        Solution ob;
+        cout << ob.countTriplet(arr, n) << "\n";
+
+    }
+
+    return 0;
+}  // } Driver Code Ends
