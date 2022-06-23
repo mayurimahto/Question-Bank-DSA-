8fec81f142065cf7d4f274a41a1bb66d77ac5369
Mayuri Mahto <mahtomayuri@gmail.com>
2022-06-23 21:29:10 +0530

    ques 5

---
+++ Arrays/Merge two sorted arrays/Merge Without Extra Space.cpp
@@ -0,0 +1,80 @@
+//Bruteforce
+
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            // code here
+            // TC- nlogn + O(n)
+            
+            vector<int>arr3;
+            
+            for(int i=0; i<n; i++)
+            {
+                arr3.push_back(arr1[i]);
+            }
+            for(int j=0; j<m; j++)
+            {
+                arr3.push_back(arr2[j]);
+            }
+            
+            sort(arr3.begin(), arr3.end());
+            
+            for(int i=0; i<n; i++)
+            {
+                arr1[i] = arr3[i];
+            }
+            for(int j=0; j<m; j++)
+            {
+                arr2[j] = arr3[n+j];
+            }
+        } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
+
+
+  
