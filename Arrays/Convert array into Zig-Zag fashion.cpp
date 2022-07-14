79694e936989e148b5a0eee4a3b2d7029df32c64
Mayuri Mahto <mahtomayuri@gmail.com>
2022-07-14 21:49:31 +0530

    ques 18

---
+++ Arrays/Convert array into Zig-Zag fashion.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Program for zig-zag conversion of array
+	void zigZag(int arr[], int n) {
+	    // code here
+	    bool flag=true;
+	    for(int k=0;k<n-1;k++)
+	    {
+	        if(flag==true)
+	        {if(arr[k]>arr[k+1])
+	        swap(arr[k],arr[k+1]);
+	        flag=false;
+	        }
+	        else if(flag==false)
+	        {if(arr[k]<arr[k+1])
+	        swap(arr[k],arr[k+1]);
+	        flag=true;
+	        }
+	    }
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.zigZag(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
